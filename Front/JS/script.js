1828ac6e092579ebcdf237e38ebc50b1d8caa935
Jenny <jenny.pothin@outlook.fr>
2021-09-23 09:01:28 +0200

    ajout commentaire

--- Front/JS/script.js
+++ Front/JS/script.js
@@ -1,26 +1,26 @@
 
 const urlCameras = "http://localhost:3000/api/cameras";
 
-    // fonction qui récupère les données de l'API et qui créer une carte produit
+
+    // fonction qui affichera la liste des produits
     function listProduct(elementArray) {
         
         for(let i of elementArray){
            
-        let infoProduct = document.querySelector('#list_product');
+        let infoProduct = document.querySelector('.card-info');
             
-        let div = document.createElement('div');
+        let div = document.createElement('div');// création d'un div 
+        div.className = "col-12 col-md-4 col-xl-3";
         const divContent=`
-        <div class="col-12 col-md-5 col-xl-3">
-        <div class="card mt-5"><a href=product.html#${i.name} class="stretched-link">
+        <div class="card mt-5"><a href=product.html?${i._id} class="stretched-link">
         <img class="card-img" src="${i.imageUrl}" alt="Un appareil photo vintage"></a>
         <div class="card-body text-center">
         <h2 class="card-title">${i.name}</h2>
         <p class="card-price">${i.price/100},00€</p>
-        <p class="card-text">${i.description}</p>
         <button type="button" class="btn btn-dark">Voir le produit</button>
         </div>
-        </div></div>`;
-        div.innerHTML=divContent;
+        </div>`;
+        div.innerHTML=divContent;// integration de la div dans le html
         infoProduct.appendChild(div);
 
         }
@@ -29,12 +29,12 @@ const urlCameras = "http://localhost:3000/api/cameras";
 // Appel de l'API
     function getApi(url) {
         fetch(url)
-        .then(function(res){
-            if(res.ok){
+        .then(function(res){ // test de la promise
+            if(res.ok){ 
                 return res.json();
             }
         })
-        .then(function(x){
+        .then(function(x){ // appel de la fonction affichage des produits
             listProduct(x);
         })
         
