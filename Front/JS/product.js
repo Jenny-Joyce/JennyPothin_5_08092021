38f85751d3c86647ac1435290305f2606cdcf617
Jenny <jenny.pothin@outlook.fr>
2021-10-15 13:40:13 +0200

    add img et desc dans la fonction addbasket

--- Front/JS/product.js
+++ Front/JS/product.js
@@ -2,6 +2,7 @@ const productId = window.location.href.split("?")[1].trim();
 const cameraUrl = "http://localhost:3000/api/cameras/"+ productId;
 console.log(cameraUrl);
 
+ 
 /* fonction qui ajout un article dans le panier
  elle retourne 0 en cas d'ajout dans le panier
  sinon retourne un code d'erreur:
@@ -15,10 +16,13 @@ function addBasket(){
     const spanId = document.querySelector("#id_product");
     const priceProduct = document.querySelector("#price");
     const inputNumber = document.querySelector('#amount');
+    const imgProduct = document.querySelector('#img').src;
+    const descProduct = document.querySelector('#description').textContent;
+    console.log(descProduct.textContent);
     let opt = elementSelect.options[elementSelect.selectedIndex].value;
-    const toAdd =  opt + "#" + priceProduct.innerHTML + "#" + inputNumber.value;
+    const toAdd = imgProduct +"#" + descProduct +"#" + opt + "#" + priceProduct.innerHTML + "#" + inputNumber.value;
     const basket = localStorage.getItem(spanId.innerHTML);
-    console.log(basket);
+    console.log(spanId);
     // controle du choix de l'option
     if(opt == "-1"){
         alert("Veuillez selectionner une option")
@@ -45,39 +49,37 @@ function addBasket(){
 
 // fonction qui affiche le produit sélectionné sur la page produit
 function displayCamera(camera){
+
     const product = document.querySelector("#container");
 
     product.className = "col col-md-6 col-xl-3"
     product.innerHTML = `
     <div class="card mt-5">
-    <span id="id_product" style="visibility:hidden">${camera._id}</span>
-    <a href="product.html?${camera._id}">
+        <span id="id_product" style="visibility:hidden">${camera._id}</span>
+        <a href="product.html?${camera._id}">
         <img class="card-img" id="img" src="${camera.imageUrl}" alt="Un appareil photo vintage"></a>
         <div class="card-body text-center">
-        <h2 class="card-title" id="name">${camera.name}</h2>
-        <p class="card-text" id="description">${camera.description}</p>
-        <select class="form-select mb-3" name="lense"></select>
-        <input type="number" id="amount" value="0" min=0 max=10>
-        <p class="card-price" id="price">${camera.price/100},00€</p>
-        <button type="button" class="btn btn-dark" id="add_basket">Ajouter au panier</button>
+            <h2 class="card-title" id="name">${camera.name}</h2>
+            <p class="card-text" id="description">${camera.description}</p>
+            <select class="form-select mb-3" name="lense"></select>
+            <input type="number" id="amount" value="0" min=0 max=10>
+            <p class="card-price" id="price">${camera.price/100},00€</p>
+            <button type="button" class="btn btn-dark" id="add_basket">Ajouter au panier</button>
         </div>
-        </div>`;
+    </div>`;
 
         let listOption = '<option selected value="-1">Selectionner un objectif</option>';
         // boucle qui affiche la liste des options
         // pour les objectifs
         for(let i in camera.lenses){ 
-            const optionLense = `<option value="${i}">${camera.lenses[i]}</option>`
+            const optionLense = `<option value="${camera.lenses[i]}">${camera.lenses[i]}</option>`
             listOption += optionLense;
-            console.log(listOption);
         }
         let selectOption = document.querySelector('.form-select');
         selectOption.innerHTML = listOption;
         
         const buttonBasket = document.querySelector('#add_basket');
-        buttonBasket.addEventListener('click', addBasket)
-        
-
+        buttonBasket.addEventListener('click', addBasket);
 }
 // appel de l'API avec l'id produit
 function idApi(url){
